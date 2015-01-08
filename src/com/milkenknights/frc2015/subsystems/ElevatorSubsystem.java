ed13321476cc810afd4da339688e4292bc42dd0c
Daniel Kessler <sinani201@gmail.com>
2015-01-08 15:43:01 -0800

    replaced tabs with spaces

--- src/com/milkenknights/frc2015/subsystems/ElevatorSubsystem.java
+++ src/com/milkenknights/frc2015/subsystems/ElevatorSubsystem.java
@@ -7,36 +7,36 @@ import edu.wpi.first.wpilibj.CANTalon;
  */
 
 public class ElevatorSubsystem {
-	public enum Positions {GROUND, SCORINGPLATFORM, STEP, FIRSTTOTE, SECONDTOTE, THIRDTOTE};
-	Positions elevatorPosition;
-	
-	CANTalon elevatorTalon;
-	
-	
-	public ElevatorSubsystem() {
-		elevatorTalon = new CANTalon(0);
-		
-		elevatorTalon.changeControlMode(CANTalon.ControlMode.Position);
-	}
-	
-	public void setPosition(Positions position) {
-		elevatorPosition = position;
-	}
-	
-	public void update(){
-		switch (elevatorPosition) {
-		case GROUND:
-			elevatorTalon.set(0);
-		case SCORINGPLATFORM:
-			elevatorTalon.set(1);
-		case STEP:
-			elevatorTalon.set(2);
-		case FIRSTTOTE:
-			elevatorTalon.set(3);
-		case SECONDTOTE:
-			elevatorTalon.set(4);
-		case THIRDTOTE:
-			elevatorTalon.set(5);
-		}
-	}
+    public enum Positions {GROUND, SCORINGPLATFORM, STEP, FIRSTTOTE, SECONDTOTE, THIRDTOTE};
+    Positions elevatorPosition;
+
+    CANTalon elevatorTalon;
+
+
+    public ElevatorSubsystem() {
+        elevatorTalon = new CANTalon(0);
+
+        elevatorTalon.changeControlMode(CANTalon.ControlMode.Position);
+    }
+
+    public void setPosition(Positions position) {
+        elevatorPosition = position;
+    }
+
+    public void update(){
+        switch (elevatorPosition) {
+        case GROUND:
+            elevatorTalon.set(0);
+        case SCORINGPLATFORM:
+            elevatorTalon.set(1);
+        case STEP:
+            elevatorTalon.set(2);
+        case FIRSTTOTE:
+            elevatorTalon.set(3);
+        case SECONDTOTE:
+            elevatorTalon.set(4);
+        case THIRDTOTE:
+            elevatorTalon.set(5);
+        }
+    }
 }
