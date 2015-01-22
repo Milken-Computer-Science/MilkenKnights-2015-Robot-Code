570029329fe29895aa087b6c9c1145b21ad31d81
austinshalit <austinshalit@gmail.com>
2015-01-21 18:54:02 -0800

    Updated Constants

--- src/com/milkenknights/frc2015/Constants.java
+++ src/com/milkenknights/frc2015/Constants.java
@@ -3,14 +3,14 @@ package com.milkenknights.frc2015;
 /** A listing of constants/settings used throughout the robot code. */
 public class Constants {
     // CAN Device Numbers
-    public static final int leftTalonDeviceNumberA = 1;
-    public static final int leftTalonDeviceNumberB = 2;
-    public static final int leftTalonDeviceNumberC = 3;
-    public static final int rightTalonDeviceNumberA = 4;
-    public static final int rightTalonDeviceNumberB = 5;
-    public static final int rightTalonDeviceNumberC = 6;
+    public static final int leftTalonDeviceNumberA = 3; // CIM
+    public static final int leftTalonDeviceNumberB = 4; // CIM
+    public static final int leftTalonDeviceNumberC = 5; // 550
+    public static final int rightTalonDeviceNumberA = 8; // CIM
+    public static final int rightTalonDeviceNumberB = 7; // CIM
+    public static final int rightTalonDeviceNumberC = 6; // 550
     
-    public static final int elevatorTalonDeviceNumber = 7;
+    public static final int elevatorTalonDeviceNumber = 0;
     
     // DIO Ports
     public static final int pressureTransducerChannel = 0;
