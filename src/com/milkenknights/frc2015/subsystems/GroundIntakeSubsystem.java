5537bbadc759dd918fad0a280f855817cdd627e5
Jake Reiner <jtreiner123@gmail.com>
2015-02-17 17:34:57 -0800

    Rewrote GroundIntakeSubsystem

--- src/com/milkenknights/frc2015/subsystems/GroundIntakeSubsystem.java
+++ src/com/milkenknights/frc2015/subsystems/GroundIntakeSubsystem.java
@@ -7,14 +7,19 @@ import com.milkenknights.frc2015.Constants;
 import edu.wpi.first.wpilibj.CANTalon;
 
 public class GroundIntakeSubsystem extends MSubsystem {
-
     CANTalon rightTalon;
     CANTalon leftTalon;
+    
+    boolean actuatorsState;
     
     /** when this is false, the actuators are opened */
     RestrictedSolenoidPair actuators;
     
-    boolean wheelsSpinning;
+    enum WheelsState {
+        FORWARD, BACKWARD, STOPPED
+    }
+    
+    WheelsState wheelsState;
     
     public GroundIntakeSubsystem() {
         leftTalon = new CANTalon(Constants.groundIntakeLeftTalonDeviceNumber);
@@ -26,35 +31,32 @@ public class GroundIntakeSubsystem extends MSubsystem {
     }
     
     /**
-     * Start spinning the wheels.
+     * Start moving the wheels forward, backward, or stop them.
+     * @param wheelsState The desired state of the wheels
      */
-    public void spinWheels() {
-        wheelsSpinning = true;
+    public void setWheelsState(WheelsState wheelsState) {
+        this.wheelsState = wheelsState;
     }
     
     /**
-     * Opens the actuators and stops the wheels.
+     * Change the state of the actuators
+     * @param s If this is true, close the actuators
      */
-    public void open() {
-        wheelsSpinning = false;
-        actuators.set(false);
-    }
-    
-    /**
-     * Closes the ground intake and starts spinning the wheels.
-     */
-    public void close() {
-        wheelsSpinning = true;
-        actuators.set(true);
+    public void setActuators(boolean s) {
+        actuatorsState = s;
     }
     
     public void update() {
-        if (wheelsSpinning) {
+        if (wheelsState == WheelsState.FORWARD) {
             leftTalon.set(Constants.groundIntakeTalonSpeed);
             rightTalon.set(Constants.groundIntakeTalonSpeed);
+        } else if (wheelsState == WheelsState.BACKWARD) {
+            leftTalon.set(-Constants.groundIntakeTalonSpeed);
+            rightTalon.set(-Constants.groundIntakeTalonSpeed);
         } else {
             leftTalon.set(0);
             rightTalon.set(0);
         }
+        actuators.set(actuatorsState);
     }
 }
