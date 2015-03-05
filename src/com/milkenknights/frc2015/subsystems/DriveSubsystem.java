e88577b9079c03ecf9e131af1a26b88014631778
austinshalit <austinshalit@gmail.com>
2015-03-04 17:49:24 -0800

    Updated drivesubsystem PID

--- src/com/milkenknights/frc2015/subsystems/DriveSubsystem.java
+++ src/com/milkenknights/frc2015/subsystems/DriveSubsystem.java
@@ -5,9 +5,8 @@ import com.milkenknights.common.MSubsystem;
 import com.milkenknights.frc2015.Constants;
 
 import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.CANTalon.ControlMode;
 import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.PIDController;
-import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -15,159 +14,90 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 /**
  * The subsystem that manages the robot's wheels.
  * 
- * @author Daniel Kessler
- * @author Jake Reiner
  */
 public class DriveSubsystem extends MSubsystem {
     RobotDrive drive;
-    
-    double leftSpeed;
-    double rightSpeed;
-    
-    double cheesyPower;
-    double cheesyTurn;
-    boolean cheesyQuickturn;
-    
-    double leftSpeedPID;
-    double rightSpeedPID;
-    
-    PIDController pid_l;
-    PIDController pid_r;
-    PIDController pid_pivot;
-    
+
     Encoder enc_l;
     Encoder enc_r;
-    
+
     IMU gyro;
-    
+
+    double leftSpeed;
+    double rightSpeed;
+
+    double PIDStraightSetpoint;
+    double PIDPivotSetpoint;
+
     public enum DriveMode {
-        TANK, CHEESY, PIDSTRAIGHT, PIDPIVOT
+        TANK, PIDSTRAIGHT, PIDPIVOT
     }
-    
+
     DriveMode driveMode;
-    
+
     public DriveSubsystem() {
         CANTalon leftTalonA = new CANTalon(Constants.leftTalonDeviceNumberA);
         CANTalon leftTalonB = new CANTalon(Constants.leftTalonDeviceNumberB);
-        
+
         CANTalon rightTalonA = new CANTalon(Constants.rightTalonDeviceNumberA);
         CANTalon rightTalonB = new CANTalon(Constants.rightTalonDeviceNumberB);
-        
-        leftTalonB.changeControlMode(CANTalon.ControlMode.Follower);
-        rightTalonB.changeControlMode(CANTalon.ControlMode.Follower);
-        
-        leftTalonB.set(Constants.leftTalonDeviceNumberA);
-        rightTalonB.set(Constants.rightTalonDeviceNumberA);
-                
+
         drive = new RobotDrive(leftTalonA, rightTalonA);
-               
+
         enc_l = new Encoder(Constants.driveLeftEncoderDeviceNumberA,
                 Constants.driveLeftEncoderDeviceNumberB);
         enc_r = new Encoder(Constants.driveRightEncoderDeviceNumberA,
                 Constants.driveRightEncoderDeviceNumberB);
-        
+
+        gyro = new IMU(new SerialPort(Constants.imuBaudRate,
+                SerialPort.Port.kMXP));
+
+        leftTalonB.changeControlMode(ControlMode.Follower);
+        rightTalonB.changeControlMode(ControlMode.Follower);
+
+        leftTalonB.set(leftTalonA.getDeviceID());
+        rightTalonB.set(rightTalonA.getDeviceID());
+
         enc_l.setDistancePerPulse(Constants.driveInchesPerPulse);
         enc_r.setDistancePerPulse(Constants.driveInchesPerPulse);
-        
+
         enc_l.setReverseDirection(true);
-        
+
         driveMode = DriveMode.TANK;
-        
-        gyro = new IMU(
-                new SerialPort(Constants.imuBaudRate, SerialPort.Port.kMXP));
-        
-        class LPIDOut implements PIDOutput {
-            @Override
-            public void pidWrite(double output) {
-                leftSpeedPID = output;
-            }
-        }
-        class RPIDOut implements PIDOutput {
-            @Override
-            public void pidWrite(double output) {
-                rightSpeedPID = output;
-            }
-        }
-        
-        pid_l = new PIDController(Constants.driveStraightPID.kp,
-                Constants.driveStraightPID.ki,
-                Constants.driveStraightPID.kd,
-                enc_l,
-                new LPIDOut());
-        pid_r = new PIDController(Constants.driveStraightPID.kp,
-                Constants.driveStraightPID.ki,
-                Constants.driveStraightPID.kd,
-                enc_r,
-                new RPIDOut());
-        
-        class PivotController implements PIDOutput {
-            @Override
-            public void pidWrite(double output) {
-                leftSpeedPID = output;
-                rightSpeedPID = -output;
-            }
-        }
-        
-        pid_pivot = new PIDController(Constants.drivePivotPID.kp,
-                Constants.drivePivotPID.ki,
-                Constants.drivePivotPID.kd,
-                gyro,
-                new PivotController());
-        pid_pivot.setInputRange(-180, 180);
-        pid_pivot.setContinuous(true);
     }
-    
+
     /**
      * Change the current drive mode
      * 
-     * @param mode The mode to change to
+     * @param mode
+     *            The mode to change to
      */
     public void setDriveMode(DriveMode mode) {
         driveMode = mode;
-        if (driveMode == DriveMode.PIDSTRAIGHT) {
-            pid_l.enable();
-            pid_r.enable();
-        } else {
-            safeDisablePID(pid_l);
-            safeDisablePID(pid_r);
-        }
-        
-        if (driveMode == DriveMode.PIDPIVOT) {
-            pid_pivot.enable();
-        } else {
-            safeDisablePID(pid_pivot);
-        }
     }
 
     /**
      * Set the speeds of the left and right side of the robot.
      * 
-     * @param left The desired speed of the robot's left side.
-     * @param right The desired speed of the robot's right side.
+     * @param left
+     *            The desired speed of the robot's left side.
+     * @param right
+     *            The desired speed of the robot's right side.
      */
     public void tankDrive(double left, double right) {
         leftSpeed = left;
         rightSpeed = right;
     }
-    
+
     /**
-     * Set robot speeds based on CheesyDrive.
-     * 
-     * {@inheritDoc Drive#cheesyDrive(double, double, boolean)}
-     */
-    public void cheesyDrive(double power, double turn, boolean quickturn) {
-        //drive.cheesyDrive(power, turn, quickturn);
-    }
-    
-    /**
-     * Set the setpoint for PID straight driving mode.
-     * This should be how far forward you want the robot to move.
+     * Set the setpoint for PID straight driving mode. This should be how far
+     * forward you want the robot to move.
      *
-     * @param setpoint The desired PID straight setpoint.
+     * @param setpoint
+     *            The desired PID straight setpoint.
      */
     public void setStraightPIDSetpoint(double setpoint) {
-        pid_l.setSetpoint(setpoint);
-        pid_r.setSetpoint(setpoint);
+        PIDStraightSetpoint = setpoint;
     }
 
     /**
@@ -176,17 +106,18 @@ public class DriveSubsystem extends MSubsystem {
      * @return The last set PID straight setpoint.
      */
     public double getStraightPIDSetpoint() {
-        return pid_l.getSetpoint();
+        return PIDStraightSetpoint;
     }
 
     /**
-     * Set the setpoint for PID pivot mode.
-     * This should be the angle you want the robot to be facing.
+     * Set the setpoint for PID pivot mode. This should be the angle you want
+     * the robot to be facing.
      *
-     * @param setpoint The desired PID angle setpoint, between -180 and 180.
+     * @param setpoint
+     *            The desired PID angle setpoint, between -180 and 180.
      */
     public void setPivotPIDSetpoint(double setpoint) {
-        pid_pivot.setSetpoint(setpoint);
+        PIDPivotSetpoint = setpoint;
     }
 
     /**
@@ -195,7 +126,7 @@ public class DriveSubsystem extends MSubsystem {
      * @return The last set PID pivot setpoint.
      */
     public double getPivotPIDSetpoint() {
-        return pid_pivot.getSetpoint();
+        return PIDPivotSetpoint;
     }
 
     /**
@@ -205,70 +136,74 @@ public class DriveSubsystem extends MSubsystem {
     public void resetPIDPosition() {
         enc_l.reset();
         enc_r.reset();
-        pid_pivot.reset();
+        gyro.zeroYaw();
     }
 
     /**
      * Find out if we have reached our straight PID target.
      *
-     * @param threshold How close/precise we want to be
-     * @return true if we have reached the target.
-     *         false if we are not in a PID mode at all
+     * @param threshold
+     *            How close/precise we want to be
+     * @return true if we have reached the target. false if we are not in a PID
+     *         mode at all
      */
     public boolean pidOnTarget(double threshold) {
         if (driveMode == DriveMode.PIDPIVOT) {
-            return Math.abs(pid_pivot.getError()) <= threshold;
+            return Math.abs(getPivotPIDSetpoint() - gyro.getYaw()) <= threshold;
         } else if (driveMode == DriveMode.PIDSTRAIGHT) {
-            return Math.abs(pid_l.getError()) <= threshold ||
-            Math.abs(pid_r.getError()) <= threshold;
+            return Math.abs(getStraightPIDSetpoint() - enc_l.pidGet()) <= threshold
+                    || Math.abs(getStraightPIDSetpoint() - enc_l.pidGet()) <= threshold;
         }
         return false;
     }
     
-    /**
-     * Safely disables a PID controller by first checking if it is enabled. 
-     * 
-     * @param pidController The PID controller
-     */
-    private void safeDisablePID(PIDController pidController) {
-        if (pidController.isEnable()) {
-            pidController.disable();
+    double m_error;
+    public double pivotPIDError() {
+        m_error = PIDPivotSetpoint - gyro.pidGet();
+
+        if (Math.abs(m_error) > (Constants.gyroMaximumInput - Constants.gyroMinimumInput) / 2) {
+            if (m_error > 0) {
+                m_error = m_error - Constants.gyroMaximumInput
+                        + Constants.gyroMinimumInput;
+            } else {
+                m_error = m_error + Constants.gyroMaximumInput
+                        - Constants.gyroMinimumInput;
+            }
         }
+        
+        return m_error;
     }
 
-    public void teleopInit() {
-        setDriveMode(DriveMode.TANK);
-    }
-    
     /**
      * Updates wheel speeds depending on driveMode (which should be set to the
-     * desired mode with setDriveMode().
-     * This method should be called during every loop no matter what.
+     * desired mode with setDriveMode(). This method should be called during
+     * every loop no matter what.
      */
     public void update() {
         switch (driveMode) {
         case TANK:
-            drive.tankDrive(leftSpeed, rightSpeed);
-            break;  
-        /*case CHEESY:
-            //drive.cheesyDrive(cheesyPower, cheesyTurn, cheesyQuickturn);
-            break; */
+            drive.tankDrive(leftSpeed, rightSpeed, true);
+            break;
         case PIDSTRAIGHT:
+            drive.tankDrive(Constants.driveStraightP
+                    * (getStraightPIDSetpoint() - enc_l.pidGet()),
+                    Constants.driveStraightP
+                            * (getStraightPIDSetpoint() - enc_r.pidGet()));
         case PIDPIVOT:
-            drive.tankDrive(leftSpeedPID, rightSpeedPID);
+            double m_result = Constants.drivePivotP * pivotPIDError();
+
+            if (m_result > 1) {
+                m_result = 1;
+            } else if (m_result < -1) {
+                m_result = -1;
+            }
+
+            tankDrive(m_result, -m_result);
             break;
         }
-        
+
         SmartDashboard.putNumber("l dist", enc_l.pidGet());
         SmartDashboard.putNumber("r dist", enc_r.pidGet());
-        SmartDashboard.putNumber("l setpoint", pid_l.getSetpoint());
-        SmartDashboard.putNumber("r setpoint", pid_r.getSetpoint());
         SmartDashboard.putNumber("gyro", gyro.pidGet());
-        SmartDashboard.putBoolean("l pid", pid_l.isEnable());
-        SmartDashboard.putBoolean("r pid", pid_l.isEnable());
-        SmartDashboard.putBoolean("pivot pid", pid_pivot.isEnable());
-        
-        //System.out.println("l "+enc_l.getDistance()+" r "+enc_r.getDistance()+
-        //        " a "+gyro.pidGet());
     }
 }
