d7eef144b45c3646cfff96436fdc514aec623ac3
austinshalit <austinshalit@gmail.com>
2015-03-04 17:43:39 -0800

    Updated Constants

--- src/com/milkenknights/frc2015/Constants.java
+++ src/com/milkenknights/frc2015/Constants.java
@@ -1,26 +1,17 @@
 package com.milkenknights.frc2015;
 
-import com.milkenknights.common.PIDGains;
-
 /** A listing of constants/settings used throughout the robot code. */
 public class Constants {
     // CAN Device Numbers
     public static final int groundIntakeLeftTalonDeviceNumber = 1;
     public static final int leftElevatorTalonDeviceNumber = 2;
-    public static final int leftTalonDeviceNumberA = 3; // CIM
-    public static final int leftTalonDeviceNumberB = 4; // CIM
-    public static final int leftTalonDeviceNumberC = 5; // 550
-    //public static final int rightTalonDeviceNumberC = 6; // 550
-    public static final int rightTalonDeviceNumberB = 7; // CIM
-    public static final int rightTalonDeviceNumberA = 8; // CIM
+    public static final int leftTalonDeviceNumberA = 3;
+    public static final int leftTalonDeviceNumberB = 4;
+    public static final int rightTalonDeviceNumberB = 7;
+    public static final int rightTalonDeviceNumberA = 8;
     public static final int rightElevatorTalonDeviceNumber = 9;
     public static final int groundIntakeRightTalonDeviceNumber = 10;
     public static final int binGrabberTalonDeviceNumber = 6;
-    
-    // these correspond to index numbers, so A->0, B->1, and C->2
-    // these are the wheels that should be reversed
-    public static final int[] reversedLeftTalons = {2};
-    public static final int[] reversedRightTalons = {0,1};
     
     // Analog Ports
     public static final int pressureTransducerChannel = 0;
@@ -47,17 +38,15 @@ public class Constants {
     public static final double transducerOffset = -25;
     
     public static final int imuBaudRate = 57600;
+    public static final double gyroMaximumInput = 180;
+    public static final double gyroMinimumInput = -180;
     
-    // 4 inch wheel diameter. encoder does 360 pulses per revolution
     public static final double driveInchesPerPulse = 4 * Math.PI / 360;
-    
     public static final double elevatorInchesPerPulse = 1.25 * Math.PI / 360;
     
     // PID constants for driving straight
-    public static final PIDGains driveStraightPID =
-            new PIDGains(0.012, 0.00001, 0.005);
-    public static final PIDGains drivePivotPID =
-            new PIDGains(0.0085, 0.00001, 0);
+    public static final double driveStraightP = 0.012;
+    public static final double drivePivotP = 0.0085;
     
     public static final double minimumWheelSpeed = 0.0069;
 
@@ -66,7 +55,8 @@ public class Constants {
     public static final double groundIntakeTalonSlowSpeed = .5;
     
     // Elevator PID constants
-    public static final PIDGains elevatorPID = new PIDGains(0.195, 0, 0);
+    public static final double elevatorP = 0.195;
+    public static final double elevatorSteeringP = 0.1;
     
     public static final double elevatorResetDistance = 0.25;
     public static final double elevatorMaxDistance = 35;
