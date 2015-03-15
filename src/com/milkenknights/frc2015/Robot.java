c8136da68e6d8b0e02d4f023d483146f63870785
austinshalit <austinshalit@gmail.com>
2015-03-15 12:20:02 -0600

    Organized robot imports

--- src/com/milkenknights/frc2015/Robot.java
+++ src/com/milkenknights/frc2015/Robot.java
@@ -1,17 +1,16 @@
 package com.milkenknights.frc2015;
 
 import java.util.LinkedList;
-import java.util.ListIterator;
 
-import com.milkenknights.common.AutonomousAction;
 import com.milkenknights.common.MSubsystem;
 import com.milkenknights.common.RestrictedSolenoid;
-import com.milkenknights.frc2015.controls.*;
+import com.milkenknights.frc2015.controls.AutonomousControl;
+import com.milkenknights.frc2015.controls.ControlSystem;
+import com.milkenknights.frc2015.controls.TripleATKControl;
 import com.milkenknights.frc2015.subsystems.BinGrabberSubsystem;
 import com.milkenknights.frc2015.subsystems.DriveSubsystem;
 import com.milkenknights.frc2015.subsystems.ElevatorSubsystem;
 import com.milkenknights.frc2015.subsystems.GroundIntakeSubsystem;
-import com.milkenknights.frc2015.subsystems.autonomous.*;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 
