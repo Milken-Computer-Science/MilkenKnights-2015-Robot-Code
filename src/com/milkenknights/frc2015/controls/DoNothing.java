36603fac4bb7cf38026691884edc502346a624c8
Daniel Kessler <sinani201@gmail.com>
2015-03-18 15:44:54 -0700

    changed DoNothing to actively do nothing

--- src/com/milkenknights/frc2015/controls/DoNothing.java
+++ src/com/milkenknights/frc2015/controls/DoNothing.java
@@ -3,6 +3,7 @@ package com.milkenknights.frc2015.controls;
 import com.milkenknights.frc2015.subsystems.DriveSubsystem;
 import com.milkenknights.frc2015.subsystems.ElevatorSubsystem;
 import com.milkenknights.frc2015.subsystems.GroundIntakeSubsystem;
+import com.milkenknights.frc2015.subsystems.GroundIntakeSubsystem.WheelsState;
 
 public class DoNothing extends ControlSystem {
 
@@ -12,8 +13,19 @@ public class DoNothing extends ControlSystem {
     }
 
     @Override
-    public void init() { }
+    public void init() {
+        driveSub.setDriveMode(DriveSubsystem.DriveMode.TANK);
+        driveSub.tankDrive(0, 0);
+        
+        elevatorSub.setSetpoint(elevatorSub.getPosition());
+        elevatorSub.setFlapsState(ElevatorSubsystem.ActuatorsState.CLOSED);
+        
+        groundIntakeSub.setWheelsState(WheelsState.STOPPED);
+        groundIntakeSub.setActuators(
+                GroundIntakeSubsystem.ActuatorsState.CLOSED);
+    }
 
     @Override
     public void periodic() { }
 }
+    
