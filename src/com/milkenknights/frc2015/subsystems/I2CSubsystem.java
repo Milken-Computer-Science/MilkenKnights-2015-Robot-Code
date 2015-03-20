8605b76799a9154fabea4b45e22d47d7521e709e
austinshalit <austinshalit@gmail.com>
2015-03-20 16:30:33 -0700

    Created I2CSubsystem Class

---
+++ src/com/milkenknights/frc2015/subsystems/I2CSubsystem.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.milkenknights.frc2015.subsystems;
+
+import com.milkenknights.common.MSubsystem;
+
+/**
+ * @author austinshalit
+ *
+ */
+public class I2CSubsystem extends MSubsystem {
+
+}
